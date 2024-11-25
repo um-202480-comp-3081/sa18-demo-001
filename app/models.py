2fd7c2864da1a7fef331b888359c00bfc731f66f
Katie Bridson <kbridson@memphis.edu>
2024-11-25 16:00:41 -0600

    Added model class and generated first migration

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField()
+    email = models.EmailField()
+    age = models.IntegerField()

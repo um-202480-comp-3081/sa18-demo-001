5ebcdf056d832a372c0a7f86eaea930a76a74951
Katie Bridson <kbridson@memphis.edu>
2024-11-25 16:03:36 -0600

    Added validations to model class and generated alter model migration

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 
 # Create your models here.
 class Customer(models.Model):
-    name = models.CharField()
-    email = models.EmailField()
-    age = models.IntegerField()
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    age = models.IntegerField(validators=[MinValueValidator(0)])

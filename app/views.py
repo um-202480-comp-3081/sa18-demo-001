b3fce0d0599653cddb2e5459c089b7b43e19d533
Katie Bridson <kbridson@memphis.edu>
2024-11-25 16:04:20 -0600

    Added index (list) page with names only not links

--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Customer
+
 
 # Create your views here.
+def customer_list(request):
+    customers = Customer.objects.all()
+    return render(request, "app/customer_list.html", {"customers": customers})
